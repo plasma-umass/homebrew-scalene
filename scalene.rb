9be6734776913ccc3a05e852f59dbbf4d52dd6cf
Emery Berger <emery.berger@gmail.com>
2021-06-03 07:44:01 -0400

    Testing new directory.

--- scalene.rb
+++ scalene.rb
@@ -1,7 +1,8 @@
 class Scalene < Formula
-  desc "Scalene: a high-performance CPU, GPU and memory profiler for Python"
+  desc "A high-performance CPU, GPU and memory profiler for Python"
   homepage "https://github.com/plasma-umass/scalene"
-
+  license "Apache-2.0"
+  
   head do
     url "https://github.com/plasma-umass/scalene.git", :branch => "master"
   end
@@ -22,7 +23,7 @@ class Scalene < Formula
   def runner_script; <<~EOS
 #!/usr/bin/env sh
 
-OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES DYLD_INSERT_LIBRARIES=#{lib}/libscalene.dylib PYTHONMALLOC=malloc python3 -m scalene "$@"
+OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES DYLD_INSERT_LIBRARIES=#{lib}/scalene/libscalene.dylib PYTHONMALLOC=malloc python3 -m scalene "$@"
   EOS
   end
 
