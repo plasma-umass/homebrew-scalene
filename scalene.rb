b25dee52c87271670a3e714a4a3032f66469c6ad
Emery Berger <emery.berger@gmail.com>
2021-06-02 21:15:26 -0400

    Added test, fixed description.

--- scalene.rb
+++ scalene.rb
@@ -1,21 +1,25 @@
 class Scalene < Formula
-  desc "Memory profiling library for the Scalene profiler for Python"
+  desc "Scalene: a high-performance CPU, GPU and memory profiler for Python"
   homepage "https://github.com/plasma-umass/scalene"
 
   head do
     url "https://github.com/plasma-umass/scalene.git", :branch => "master"
   end
 
+  test do
+    system "scalene --help"
+  end
+
   def install
     system "make"
 
     lib.install "scalene/libscalene.dylib"
-    
+
     (buildpath/"runner_script").write(runner_script)
     bin.install "runner_script" => "scalene"
   end
-  
-  def runner_script; <<-EOS
+
+  def runner_script; <<~EOS
 #!/usr/bin/env sh
 
 OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES DYLD_INSERT_LIBRARIES=#{lib}/libscalene.dylib PYTHONMALLOC=malloc python3 -m scalene "$@"
